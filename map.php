b705bc9e85eee9bdee1aa3eded767e6ba04593c2
dashuncel <korovkinada@ya.ru>
2017-08-09 13:33:21 +0800

    v1.0

--- map.php
+++ map.php
@@ -2,19 +2,23 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-if (isset($_POST['send'])) {
+if (isset($_POST['address'])) {
 
     $api = new \Yandex\Geo\Api();
     $api->setQuery($_POST['address']);
 
 
-    $api
-        ->setLang(\Yandex\Geo\Api::LANG_US)
-        ->load();
+    $api->setLang(\Yandex\Geo\Api::LANG_US)->load();
 
-    $response = $api->getResponse();
-    $collection = $response->getList();
+    $response = $api->getResponse()->getList();
+    $collection = [];
 
-    return $collection;
+    foreach ($response as $key => $item) {
+        $collection[$key]['address'] = $item->getAddress(); // адрес
+        $collection[$key]['latt'] = $item->getLatitude(); // широта
+        $collection[$key]['long'] = $item->getLongitude(); // долгота
+    }
+
+    echo json_encode($collection);
 }
 
