1e07fddaf2881cda45c6d12961ebeb2916a8d32d
dashuncel <korovkinada@ya.ru>
2017-08-09 12:17:27 +0800

    v1.0

--- map.php
+++ map.php
@@ -1,5 +1,20 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 if (isset($_POST['send'])) {
 
+    $api = new \Yandex\Geo\Api();
+    $api->setQuery($_POST['address']);
+
+
+    $api
+        ->setLang(\Yandex\Geo\Api::LANG_US)
+        ->load();
+
+    $response = $api->getResponse();
+    $collection = $response->getList();
+
+    return $collection;
 }
+
